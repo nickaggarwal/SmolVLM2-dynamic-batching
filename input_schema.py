6b25db998e13c030779762349bfde750333aafc2
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-02-26 10:54:48 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,19 +4,7 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["There is a fine house in the forest"]
-    },
-    'negative_prompt': {
-        'datatype': 'STRING',
-        'required': False,
-        'example': ["test"],
-        'shape': [1]
-    },
-    'width': {
-        'datatype': 'INT8',
-        'required': False,
-        'example': [ 768 ,  512 ],
-        'shape': [2]
-    },
+    }
 }
 BATCH_SIZE = 4
 BATCH_WINDOW = 5000
