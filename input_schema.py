af5381eeb18c9e0dc0ee77e77ffaa9f24209c026
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-02-14 13:40:43 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -2,8 +2,8 @@ INPUT_SCHEMA = {
     "prompt": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [1],
-        'example': ["There is a fine house in the forest"]
+        'shape': [1, 1],
+        'example': [["There is a fine house in the forest"]],
     }
 }
 BATCH_SIZE = 4
