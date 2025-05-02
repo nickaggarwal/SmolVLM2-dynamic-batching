9848100536f381d8949a8bf777f0ec2cf2db7052
Nilesh Agarwal <nick.aggarwal@gmail.com>
2025-05-03 05:03:17 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -2,14 +2,14 @@ INPUT_SCHEMA = {
     "prompt": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [1, 1],
-        'example': [["Describe the video"]],
+        'shape': [1],
+        'example': ["Describe the video"],
     },
     "video_file": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [1, 1],
-        'example': [["https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_640_3MG.mp4"]],
+        'shape': [1],
+        'example': ["https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_640_3MG.mp4"],
     }
 }
 BATCH_SIZE = 4
